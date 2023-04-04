70d07fda0079f61c400fc0b472b61d9bebe7f566
sofyzsy.st <119410757+sofijastarshaja@users.noreply.github.com>
2023-04-04 12:32:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const input1 = document.querySelector('js-teksts');
-const plusButton = document.querySelector('.js-plus')
+const input1 = document.querySelector('.js-teksts1');
+const input2 = document.querySelector('.js-teksts2');
+const plusButton = document.querySelector('.js-plus');
+const minusButton = document.querySelector('.js-minus');
+const multiplyButton = document.querySelector('.js-multiply');
+const divideButton = document.querySelector('.js-divide');
+const result = document.querySelector('.js-result');
 
 const Add = (skaitlis1, skaitlis2) => {
-    return skaitlis1 + skaitlis2
+    return Number(skaitlis1) + Number(skaitlis2)
 }
-const Subtract = (skaitlis1, skaitlis2)
-=> {
+
+const Subtract = (skaitlis1, skaitlis2) => {
     return skaitlis1 - skaitlis2
 }
-const Multiply = (skaitlis1, skaitlis2)
-=> {
+
+const Multiply = (skaitlis1, skaitlis2) => {
     return skaitlis1 * skaitlis2
 }
-const Divide = (skaitlis1, skaitlis2)
-=> {
+
+const Divide = (skaitlis1, skaitlis2) => {
     return skaitlis1 / skaitlis2
-
 }
-plusButton.addEventListener('click', () => {
-    console.log("mani nospieda")
-})
-console.log(input1)
 
-console.log(Add(22,13))
+plusButton.addEventListener('click', () => {
+    result.innerHTML = Add(Number(input1.value), Number(input2.value))
+})
